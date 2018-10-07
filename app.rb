18240de46abd18f1608f0d085fdc47a064ccd8f2
v3rba <verbush@gmail.com>
2018-10-07 17:41:35 +0300

    Add check if barber already booked

--- app.rb
+++ app.rb
@@ -4,19 +4,53 @@ require 'sinatra/reloader'
 require 'yaml/store'
 require 'sqlite3'
 
-# configure do
-#   db = get_db
-#   db.execute 'CREATE TABLE IF NOT EXISTS
-#     "Users"
-#     (
-#         "id" INTEGER PRIMARY KEY AUTOINCREMENT, 
-#         "username" TEXT, 
-#         "phone" TEXT, 
-#         "datestump" TEXT, 
-#         "barber" TEXT, 
-#         "color" TEXT
-#     )'
-# end
+def is_barber_exists? db, name
+  db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+  barbers.each do |barber|
+    if !is_barber_exists? db, barber
+      db.execute 'insert into Barbers (name) values (?)', [barber]
+    end 
+  end
+
+end
+
+def get_db
+  db = SQLite3::Database.new 'barbershop.db'
+  db.results_as_hash = true
+  return db
+end
+
+before do
+  db = get_db
+  @barbers = db.execute 'select * from Barbers'
+end
+
+configure do
+  db = get_db
+  db.execute 'CREATE TABLE IF NOT EXISTS
+    "Users"
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "username" TEXT,
+      "phone" TEXT,
+      "datestamp" TEXT,
+      "barber" TEXT,
+      "color" TEXT
+    )'
+
+  db.execute 'CREATE TABLE IF NOT EXISTS
+    "Barbers"
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "name" TEXT
+    )'
+
+  seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified by <a href=\"https://github.com/v3rba\">Verba</a>"
@@ -51,17 +85,17 @@ post '/visit' do
     return erb :visit
   end
 
-  # db = get_db
-  # db.execute 'insert into 
-  #   Users 
-  #   (
-  #     username,
-  #     phone,
-  #     datestamp,
-  #     barber,
-  #     color
-  #   )
-  #   values ( ?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+  db = get_db
+  db.execute 'insert into
+    Users
+    (
+      username,
+      phone,
+      datestamp,
+      barber,
+      color
+    )
+    values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
   erb "Ok, See you soon!"
 
@@ -87,3 +121,8 @@ post '/contacts' do
   erb "Thank you"
 
 end
+
+get '/showusers' do
+  erb "Hello World"
+end
+
